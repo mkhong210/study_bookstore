5aa3c98f371b4427263bd6faff7411ed458c2bfc
mkhong210 <mkhong210@gmail.com>
2025-05-23 17:38:29 +0900

    fix: 리스트 페이지 UI 및 기능 개선

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,9 +1,47 @@
-// 25.05.22
+// 25.05.23
+import axios from "axios";
+import { useEffect, useState } from "react";
+import bookdata from "../data/Bookdata";
+import { Container, Row } from "react-bootstrap";
+import BookItem from "../components/BookItem";
 
 const ListPage: React.FC = () => {
+  const [books, setBooks] = useState(bookdata);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    if (!loaded) {
+      axios
+        .get("https://jamsuham75.github.io/image/data2.json")
+        .then((result) => {
+          console.log(result.data);
+          console.log(books);
+          const copy = [...books, ...result.data];
+
+          setBooks(copy);
+          console.log(copy);
+          setLoaded(true);
+        })
+        .catch(() => {
+          console.log("fail");
+        });
+    }
+  }, [loaded, books]);
   return (
     <div>
-      <p>도서 목록 페이지</p>
+      <Container>
+        <div className="project_header_container">
+          <h1 className="project_header">도서 리스트</h1>
+          <div className="hr"></div>
+        </div>
+        <div>
+          <Row className="text-center">
+            {books.map((item, i) => {
+              return <BookItem key={i} book={item}></BookItem>;
+            })}
+          </Row>
+        </div>
+      </Container>
     </div>
   );
 };
