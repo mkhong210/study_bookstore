28ae7a13aa791c622c79c14851e6f7aa700074c5
mkhong210 <mkhong210@gmail.com>
2025-05-26 19:00:32 +0900

    feat: Contact 페이지 구현

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,10 +1,38 @@
-// 25.05.22
+// 25.05.26
+
+import { Button, Container, Form } from "react-bootstrap";
 
 const ContactPage: React.FC = () => {
   return (
-    <div>
-      <p>연락처 페이지</p>
-    </div>
+    <Container>
+      <h1 className="text-center">Business Contact</h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="formName">
+          <Form.Label className="text-start w-100">이름</Form.Label>
+          <Form.Control type="text" placeholder="이름 입력"></Form.Control>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formName">
+          <Form.Label className="text-start w-100">이메일</Form.Label>
+          <Form.Control type="text" placeholder="이메일 입력"></Form.Control>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formName">
+          <Form.Label className="text-start w-100">내용</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={10}
+            placeholder="내용 입력"
+          ></Form.Control>
+        </Form.Group>
+        <Button
+          type="submit"
+          variant="warning"
+          className="w-100"
+          style={{ height: "50px" }}
+        >
+          접수하기
+        </Button>
+      </Form>
+    </Container>
   );
 };
 
